53c4dad296c721ea48f0fbfec0fba680ce89ff5f
wael2000 <waelibrahim2000@hotmail.com>
2017-01-05 10:54:55 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<h1> Hello Red Hat </h1>
-<?php
-
-// Show all information, defaults to INFO_ALL
-phpinfo();
-
-// Show just the module information.
-// phpinfo(8) yields identical results.
-phpinfo(INFO_MODULES);
-
-?>
-<h2> hello </h2>
+<H1>Version A</H1>
+<img src="a-abtesting.png">
+<div>
+<?php 
+ echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
+ ?>
+</div>
