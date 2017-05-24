22638e002dccd7d00e06b1fdd4873d79a9144c15
wael2000 <waelibrahim2000@hotmail.com>
2017-05-24 14:32:04 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 ?>
-<H1>Version A</H1>
+<H1>Version A 1.1</H1>
 <img src="a-abtesting.png">
 <div>
 <?php 
