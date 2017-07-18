eb3796574743cf1e344e560fcf173d7ce235e98e
wael2000 <waelibrahim2000@hotmail.com>
2017-07-18 16:56:12 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 ?>
 <H1>Version A </H1>
-<H3> Hellow ELM </H1>
+<H3> Hellow World </H1>
 <img src="a-abtesting.png">
 <div>
 <?php 
